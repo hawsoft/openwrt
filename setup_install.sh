0afb656ce067c2b503150b08a44c77f2d02cf2ba
lorain.li <lorain.li@qq.com>
2025-11-29 22:41:29 +0800

    new file:   setup_install.sh

---
+++ setup_install.sh
@@ -0,0 +1,7 @@
+./script/feeds clean
+./script/feeds update -a
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+./script/feeds install -a
