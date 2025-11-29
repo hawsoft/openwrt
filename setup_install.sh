ecb265d719f0b157fc139194fddc8ec50cbdd4c4
lorain.li <lorain.li@qq.com>
2025-11-30 00:44:03 +0800

    fix xray-core, v2ray-core install

--- setup_install.sh
+++ setup_install.sh
@@ -4,3 +4,4 @@ rm -rf feeds/packages/net/{mosdns,xray*,v2ray*,v2ray*}
 rm -rf feeds/packages/lang/golang
 mv feeds/collect/golang feeds/packages/lang/golang
 ./scripts/feeds install -a
+./scripts/feeds install -a -f -p collect
