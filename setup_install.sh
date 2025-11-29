9df9396cd8d8815b099e78040a7b44ec69ea35e7
lorain.li <lorain.li@qq.com>
2025-11-29 23:21:46 +0800

    mv golang 1.25 from collect to packages

--- setup_install.sh
+++ setup_install.sh
@@ -1,7 +1,8 @@
-./script/feeds clean
-./script/feeds update -a
+./scripts/feeds clean
+./scripts/feeds update -a
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
-./script/feeds install -a
+mv feeds/collect/golang feeds/packages/lang/golang
+./scripts/feeds install -a
