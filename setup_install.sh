50b060062ff9feefaafd5146230ee610ff084495
lorain.li <lorain.li@qq.com>
2025-11-29 23:43:01 +0800

    just remove pkg repeated

--- setup_install.sh
+++ setup_install.sh
@@ -1,8 +1,6 @@
 ./scripts/feeds clean
 ./scripts/feeds update -a
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/net/{mosdns,xray*,v2ray*,v2ray*}
 rm -rf feeds/packages/lang/golang
 mv feeds/collect/golang feeds/packages/lang/golang
 ./scripts/feeds install -a
